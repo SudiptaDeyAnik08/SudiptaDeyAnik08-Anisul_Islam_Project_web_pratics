bb54b9fc87df97ea93968338aeef1988eb9e94ac
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-10-13 00:08:40 +0600

    Bundle Of Project Which Learn from Anisul Islam Sir

---
+++ p1/ad.js
@@ -0,0 +1,15 @@
+var b = document.getElementById('abc');
+
+
+function alertFun1(){
+   
+    b.innerHTML = 'You Have Clicked in 1 ';
+}
+
+
+function alertFun2(){
+    
+
+    b.innerHTML = 'You Have Clicked in 2 ';
+
+}
---
+++ p2/abc.js
@@ -0,0 +1,9 @@
+const img_setter = document.getElementById("img-setter");
+
+function im(){
+    img_setter.src='a.jpg';
+}
+
+function pro(){
+    img_setter.src='b.jpg';
+}
